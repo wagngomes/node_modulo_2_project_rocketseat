b24cc7dab1c4c2fe2234637792d04cfd99b12078
wagngomes <wagner.gomes.cx2@gmail.com>
2024-12-21 10:39:23 -0300

    feat: validação de variáveis de ambiente com zod

---
+++ env/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+
+    DATABASE_URL: z.string()
+
+})
+
+export const _env = envSchema.safeParse(process.env)
+
+if(_env.success === false){
+    console.error('Variaveel de ambiente não encontrada');
+    throw new Error('invalid environment var')
+}
+
+export const env = _env.data
