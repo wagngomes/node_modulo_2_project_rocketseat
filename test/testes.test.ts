8e9e4d51f862460562acdea84cc2bc037d8cec65
wagngomes <wagner.gomes.cx2@gmail.com>
2025-01-25 11:53:55 -0300

    feat: segundo teste ok

--- test/testes.test.ts
+++ test/testes.test.ts
@@ -1,9 +1,11 @@
-import { expect, vitest, beforeAll, test, describe } from "vitest";
+import { expect, vitest, beforeAll, test, describe, afterAll, beforeEach} from "vitest";
 import supertest from "supertest";
 import { app } from "../src/app";
 import request from "supertest";
+import { execSync } from "node:child_process";
 
 describe("transactionRoutes", () => {
+
   beforeAll(async () => {
     await app.ready();
   });
@@ -12,7 +14,12 @@ describe("transactionRoutes", () => {
     await app.close();
   });
 
-  test("teste se o usuário consegue fazer uma chamada HTTP", async () => {
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  test('o usuário deve conseguir fazer uma chamada HTTP', async () => {
     const response = await request(app.server)
       .post("/transactions")
       .send({
@@ -22,4 +29,33 @@ describe("transactionRoutes", () => {
       })
       .expect(201);
   });
+
+  test('deve ser possível listar todas as transações', async() => {
+    const createTransactionResponse = await request(app.server)
+    .post("/transactions")
+    .send({
+      title: "new transaction",
+      amount: 5000,
+      type: "credit",
+    })
+    const cookies = createTransactionResponse.get('Set-Cookie')
+    if(!cookies){
+      throw new Error('cookies não encontrados')
+    }
+
+
+    const listTransactionsResponse = await request(app.server)
+    .get("/transactions")
+    .set('Cookie', cookies)
+    .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: "new transaction",
+        amount: 5000,
+
+      })
+    ])
+
+  })
 });
